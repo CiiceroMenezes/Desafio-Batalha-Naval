6734c858a606fd208a2e88c70a59acbf699a35ce
CiiceroMenezes <cicero.lopes.menezes@gmail.com>
2025-05-31 00:45:43 +0000

    Iniciando nivel novato

---
+++ desafionovato.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main ()
+{ 
+    int linhas = 10;
+    int colunas = 10;
+    int matriz[10][10] = {0};
+
+    int linhaHorizontal = 5;
+    int colunaHorizontalInicio = 2;
+
+    int colunaVertical = 7;
+    int linhaVerticalInicio = 4;
+
+    for (int j = colunaHorizontalInicio; j < colunaHorizontalInicio + 3; j++)
+    {
+        /* code */
+    }
+    
+    
+
+    
+    
+    
+
+
+
+
+
+
+    return 0;
+}
