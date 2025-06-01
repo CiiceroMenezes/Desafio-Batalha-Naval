fb127691246c5e15dbf72555cbb6bd1f29faf1b8
CiiceroMenezes <cicero.lopes.menezes@gmail.com>
2025-06-01 01:33:42 +0000

    Nivel novato e aventureiro finalizado

--- desafioaventureiro.c
+++ desafioaventureiro.c
@@ -1,3 +1,5 @@
+// Posicionar Quatro Navios: dois navios horizontal ou vertical e dois navios na diagonal
+
 #include <stdio.h>
 
 #define linhas 10
@@ -5,41 +7,62 @@
 
 int main()
 {
-    int matriz[10][10] = {0}; // Criando Array Bidimensional, mas todos com o valor "0", que representam a água.
+    int matriz[10][10] = {0}; // O valor 0 representa a água
 
-    int linhaHorizontal = 5; // Representando onde iniciará o barco Horizontal
+    int linhaHorizontal = 5; // Dando inicio a localização do navio horizontal
     int colunaHorizontalInicio = 2;
 
-    int colunaVertical = 7; // Representando onde iniciará o barco Vertical
+    int colunaVertical = 7; // Dando inicio a localização do navio vertical
     int linhaVerticalInicio = 4;
 
-    for (int j = colunaHorizontalInicio; j < colunaHorizontalInicio + 3; j++) // Definindo onde ficará localizado
+    for (int j = colunaHorizontalInicio; j < colunaHorizontalInicio + 3; j++) // Acrescentando o mesmo ao tabuleiro (horizontal)
     {
-        if (j >= 0 && j < colunas) // Verificando se esta dentro das linhas e colunas propostas
+        if (j >= 0 && j < colunas) // Verificando se esta dentro da matriz
         {
-            matriz[linhaHorizontal][j] = 3;
+            matriz[linhaHorizontal][j] = 3; // Dando o valor que representa o navio (3)
         }
     }
 
-    for (int i = linhaVerticalInicio; i < linhaVerticalInicio + 3; i++) // Definindo onde ficará localizado
+    for (int i = linhaVerticalInicio; i < linhaVerticalInicio + 3; i++) // Acrescentando o mesmo ao tabuleiro (vertical)
     {
-        if (i >= 0 && i < colunas) // Verificando se esta dentro das linhas e colunas propostas
+        if (i >= 0 && i < linhas) // Verificando se esta dentro da matriz
         {
             matriz[i][colunaVertical] = 3;
         }
     }
 
-    printf("### Tabuleiro de Batalha Naval ###\n"); // Mensagem inicial de apresentação do tabuleiro
-    for (int i = 0; i < linhas; i++)                // Criando as linhas
+    // Primeiro Navio na Diagonal
+    int linhaDiagonal1 = 1; // Decidindo onde ele vai ficar
+    int colunaDiagonal1 = 1;
+    for (int k = 0; k < 3; k++) // Variavel K para ser uma variavel auxiliar
     {
-        for (int j = 0; j < colunas; j++) // Criando as colunas
+        if ((linhaDiagonal1 + k) < linhas && (colunaDiagonal1 + k) < colunas) // Mesma verificação anterior se encontra-se dentro da matriz
         {
-            printf("%d ", matriz[i][j]); // Imprimindo na tela as colunas
+            matriz[linhaDiagonal1 + k][colunaDiagonal1 + k] = 3; // Dando o valor ao Navio (3)
         }
-        printf("\n"); // Pulando a linha para as Linhas
     }
 
-    
+    // Segundo Navio na Diagonal
+    int linhaDiagonal2 = 8;
+    int colunaDiagonal2 = 1;
+    for (int k = 0; k < 3; k++) // Usando decremento
+    {
+        if ((linhaDiagonal2 - k) >= 0 && (colunaDiagonal2 + k) < colunas)
+        {
+            matriz[linhaDiagonal2 - k][colunaDiagonal2 + k] = 3;
+        }
+    }
+
+    // Imprimindo o Tabuleiro na tela
+    printf("### Tabuleiro de Batalha Naval ###\n");
+    for (int i = 0; i < linhas; i++)
+    {
+        for (int j = 0; j < colunas; j++)
+        {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
